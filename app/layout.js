a8823217f7f320e416d8e7ab734b942e048fd166
Abhishek Yadav <yabhishekk480@gmail.com>
2026-02-20 23:24:54 +0530

    Changed tech  to next.js

---
+++ app/layout.js
@@ -0,0 +1,29 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Portfolio - Abhishek",
+  description: "Full-stack developer portfolio showcasing projects, skills, and experience in  modern web technologies.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
